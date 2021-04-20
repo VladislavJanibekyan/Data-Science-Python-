c45e4cf48be76e1e9369973e9cefe2652121736b
Vladislav <v.janibekyan@gmail.com>
2021-04-20 16:05:36 +0300

    some modifications

--- first_month/task1.3.3.py
+++ first_month/task1.3.3.py
@@ -12,11 +12,11 @@ print(fact(5))
 # Write a Python function which returns the n-th element of the fibonacci series. 
 def fibo(n):
     if n == 1:
-        return 1
+        return 0
     elif n == 2:
-        return 2
+        return 1
     elif n > 2:
         return fibo(n-1) + fibo(n-2)
         
 
-print(fibo(5))
+print(fibo(7))
