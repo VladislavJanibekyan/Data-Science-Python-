df349f28893427c1610051d4be0df7302cfbf048
Vladislav <v.janibekyan@gmail.com>
2021-05-08 12:06:10 +0300

    adding new material

--- src/first_month/task1_5_2.py
+++ src/first_month/task1_5_2.py
@@ -1,26 +1,35 @@
 class Money:
     def __init__(self, amount_init:int,currency_initialization:str):
             self.amount_init = amount_init
+            try:
+                self.amount_init +=1
+            except:
+                self.amount_init = int(input("please enter valid amount"))
+            if self.amount_init < 0:
+                self.amount_init = int(input("it can not be negative, give positive"))
             self.currency_initialization = currency_initialization
 
-    def print_obj(self):
-        return print(str(self.amount_init) + self.currency_initialization)
-    def sum_mon(self,second_obj):
-        result = self.amount_init + second_obj.amount_init 
+
+    def __repr__(self):
+        return str(self.amount_init) + self.currency_initialization
+    def __add__(self,other):
+        result = self.amount_init + other.amount_init 
         return Money(result,self.currency_initialization)
 
-    def sub_mon(self,second_obj):
-        result = abs(self.amount_init - second_obj.amount_init)
+    def __sub__(self,other):
+        result = abs(self.amount_init - other.amount_init)
         return Money(result,self.currency_initialization)
 
 def main():
-    obj_1 = Money(25, "USD")
-
+    obj_1 = Money(-3, "USD")
     obj_2 = Money(2, "USD")
-    obj_1.print_obj()
-    obj_2.print_obj()
-    obj_1.sum_mon(obj_2).print_obj()
-    obj_1.sub_mon(obj_2).print_obj()
+    print(obj_1)
+    print(obj_2)
+    print(obj_1 + obj_2)
+    print(obj_1 - obj_2)
+    
+  
+
 
 
 
